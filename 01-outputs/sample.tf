35ca9097b256bb2f3b56c1b9f36abcb8b30152a8
nikkaushal <nikkaushal@gmail>
2021-01-06 23:34:59 -0500

    Terraform

--- 01-outputs/sample.tf
+++ 01-outputs/sample.tf
@@ -1,3 +1,18 @@
 output "a" {
-  value = "100"
+  value = 100
 }
+
+output "b" {
+  value = "test the terraform"
+}
+
+output "c" {
+value = "True"
+}
+
+
+
+#Datatypes
+# String - should be in quotes
+# Numbers/float (no quotes)
+# booleans (no quotes)
