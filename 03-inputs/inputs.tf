83ac49b39a2a641c1d07dbca8f7f6a6d7c274234
nikkaushal <nikkaushal@gmail>
2021-01-07 22:44:34 -0500

    Terraform

---
+++ 03-inputs/inputs.tf
@@ -0,0 +1,11 @@
+#inputs should be loaded in variables
+#Thats why we need to declare a empty variable in terraform
+
+variable "abc" {}
+
+#input type -after terraform apply execution
+
+
+output "abc" {
+  value = var.abc
+}
