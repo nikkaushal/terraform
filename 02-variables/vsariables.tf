faca5b37cce1efb8b770095762e74500bd6806cb
nikkaushal <nikkaushal@gmail>
2021-01-07 20:49:32 -0500

    Terraform

--- 02-variables/vsariables.tf
+++ 02-variables/vsariables.tf
@@ -5,3 +5,29 @@ variable "a" {
 output "a" {
   value = var.a
 }
+
+## varianles are 3 types
+
+variable "normal" {
+  default =10
+}
+
+variable "list" {
+  default = [400, "Abgdg", true ]
+}
+
+
+## in one variables you can give multiple values which is of different data types
+
+output "list_1" {
+  value = var.list[0]
+}
+
+output "list_2" {
+  value = var.list[1]
+}
+
+output "list_3" {
+  value = var.list[2]
+}
+
