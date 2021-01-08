c87c097dcc96098037bc487605073a8ca08a62a0
nikkaushal <nikkaushal@gmail>
2021-01-07 20:55:18 -0500

    Terraform

--- 02-variables/vsariables.tf
+++ 02-variables/vsariables.tf
@@ -7,17 +7,22 @@ output "a" {
 }
 
 ## varianles are 3 types
-
+#variable type - default
 variable "normal" {
   default =10
 }
-
+#default tpye= list
 variable "list" {
   default = [400, "Abgdg", true ]
 }
 
 
 ## in one variables you can give multiple values which is of different data types
+
+output "list_1" {
+  value = var.list
+}
+
 
 output "list_1" {
   value = var.list[0]
