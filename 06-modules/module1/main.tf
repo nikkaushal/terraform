1b0cf1d3ede2afe80f3ebbf2fea0d64559b2337a
nikkaushal <nikkaushal@gmail>
2021-01-09 18:07:41 -0500

    modules

--- 06-modules/module1/main.tf
+++ 06-modules/module1/main.tf
@@ -8,3 +8,7 @@ resource "aws_instance" "sample1" {
   ami                        = "ami-052ed3344670027b3"
   instance_type              = "t3.micro"
 }
+
+output "PUBLIC_IP" {
+  value = aws_instance.sample.public_ip
+}
