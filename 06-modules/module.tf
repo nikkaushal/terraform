9f3edd9d6478bbe3e8f5233209a5fd269f8dfcfb
nikkaushal <nikkaushal@gmail>
2021-01-09 18:09:26 -0500

    modules

--- 06-modules/module.tf
+++ 06-modules/module.tf
@@ -1,5 +1,5 @@
 #module sources- https://www.terraform.io/docs/modules/sources.html
-
+#this is root module
 
 module "module1" {
   source          = "./module1"
@@ -10,3 +10,6 @@ provider "aws" {
   region="us-east-1"
 }
 
+output "PUBLIC_IP" {
+  value = module.module1.PUBLIC_IP
+}
