53eab5f50b46772ace039edc7d1ddcb4c75426f8
nikkaushal <nikkaushal@gmail>
2021-01-10 21:03:40 -0500

    functions

---
+++ 08-data-sources/sample.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "aws_ami" "example" {
+  most_recent = true
+  owners = ["811742389611"]
+  }
+}
+
+output "ami" {
+  value = data.aws_ami.example.id
+
+}
