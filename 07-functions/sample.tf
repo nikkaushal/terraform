f77b6362710a74112b36da5608a54b5d419211b0
nikkaushal <nikkaushal@gmail>
2021-01-10 19:51:26 -0500

    functions

--- 07-functions/sample.tf
+++ 07-functions/sample.tf
@@ -3,10 +3,25 @@
 variable "name" {
   default = "sampleisdevelopment"
 }
+
 resource "aws_instance" "sample" {
   ami                        = "ami-052ed3344670027b3"
   instance_type              = "t3.small"
   tags                       = {
       Name                   = upper(var.name)
   }
+}
+
+variable "image_id" {
+  type        = string
+  description = "The id of the machine image (AMI) to use for the server."
+
+  validation {
+    condition     = length(var.image_id) > 4 && substr(var.image_id, 0, 4) == "ami-"
+    error_message = "The image_id value must be a valid AMI id, starting with \"ami-\"."
+  }
+}
+
+provider "aws" {
+  region="us-east-1"
 }
